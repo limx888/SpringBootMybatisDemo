bfc01cb88ffdbcd98900eeffd91c605e8cdc4c92
ehmatthes <eric@example.com>
2018-10-12 16:15:53 +0800

    整合SpringDataJpa添加说明

--- src/main/java/com/demo/repository/AddressRepository.java
+++ src/main/java/com/demo/repository/AddressRepository.java
@@ -6,5 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AddressRepository extends JpaRepository<Addresslist, Integer> {
+    /**
+     * 根据用户名查询用户信息
+     *
+     * @param name 用户名
+     * @return 查询结果
+     */
     List<Addresslist> findAllByName(String name);
 }
