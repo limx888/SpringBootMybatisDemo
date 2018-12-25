b1a57067058b5fb09361464f8b4d1b874d2af61f
limingxia <limingxia@bocservices.com.cn>
2018-12-25 16:45:23 +0800

    actuator服务监控与管理

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -1,8 +1,13 @@
 package com.demo;
 
+import com.demo.endpoint.MyEndPoint;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.endpoint.condition.ConditionalOnEnabledEndpoint;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -15,5 +20,15 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Configuration
+    static class MyEndpointConfiguration {
+        @Bean
+        @ConditionalOnMissingBean
+        @ConditionalOnEnabledEndpoint
+        public MyEndPoint myEndPoint() {
+            return new MyEndPoint();
+        }
     }
 }
