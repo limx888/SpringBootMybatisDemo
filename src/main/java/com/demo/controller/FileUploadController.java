41b1374ad9e60fdbb99581f6287c900489685975
limingxia <limingxia@bocservices.com.cn>
2019-01-02 15:36:58 +0800

    添加文件上传

---
+++ src/main/java/com/demo/controller/FileUploadController.java
@@ -0,0 +1,91 @@
+package com.demo.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Base64Utils;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 本地测试路径：http://localhost:8080/test/uploads
+ */
+@Log4j2
+@Controller
+@RequestMapping("/uploads")
+public class FileUploadController {
+    @GetMapping
+    public String upload() {
+        return "upload";
+    }
+
+    @PostMapping("/upload1")
+    @ResponseBody
+    public Map<String, String> upload1(@RequestParam("file") MultipartFile file) throws IOException {
+        log.info("[文件类型] - [{}]", file.getContentType());
+        log.info("[文件名称] - [{}]", file.getOriginalFilename());
+        log.info("[文件大小] - [{}]", file.getSize());
+        // TODO 将文件写入到指定目录（具体开发中有可能是将文件写入到云存储/或者指定目录通过 Nginx 进行 gzip 压缩和反向代理，此处只是为了演示故将地址写成本地电脑指定目录）
+        File filePath = new File("D:/test/");
+        if (!filePath.exists()) {
+            filePath.mkdir();
+        }
+        file.transferTo(new File("D:/test/" + file.getOriginalFilename()));
+        Map<String, String> result = new HashMap<>(16);
+        result.put("contentType", file.getContentType());
+        result.put("fileName", file.getOriginalFilename());
+        result.put("fileSize", file.getSize() + "");
+        return result;
+    }
+
+    @PostMapping("/upload2")
+    @ResponseBody
+    public List<Map<String, String>> upload2(@RequestParam("file") MultipartFile[] files) throws IOException {
+        if (files == null || files.length == 0) {
+            return null;
+        }
+        List<Map<String, String>> results = new ArrayList<>();
+        for (MultipartFile file : files) {
+            // TODO Spring Mvc 提供的写入方式
+            File filePath = new File("D:/test/");
+            if (!filePath.exists()) {
+                filePath.mkdir();
+            }
+            file.transferTo(new File("D:/test/" + file.getOriginalFilename()));
+            Map<String, String> map = new HashMap<>(16);
+            map.put("contentType", file.getContentType());
+            map.put("fileName", file.getOriginalFilename());
+            map.put("fileSize", file.getSize() + "");
+            results.add(map);
+        }
+        return results;
+    }
+
+    /**
+     * 图片转base64：http://base64.xpcha.com/pic.html
+     * @param base64
+     * @throws IOException
+     */
+    @PostMapping("/upload3")
+    @ResponseBody
+    public void upload2(String base64) throws IOException {
+        // TODO BASE64 方式的 格式和名字需要自己控制（如 png 图片编码后前缀就会是 data:image/png;base64,）
+        File filePath = new File("D:/test/");
+        if (!filePath.exists()) {
+            filePath.mkdir();
+        }
+        final File tempFile = new File("D:/test/test.jpg");
+        // TODO 防止有的传了 data:image/png;base64, 有的没传的情况
+        String[] d = base64.split("base64,");
+        final byte[] bytes = Base64Utils.decodeFromString(d.length > 1 ? d[1] : d[0]);
+        FileCopyUtils.copy(bytes, tempFile);
+
+    }
+}
